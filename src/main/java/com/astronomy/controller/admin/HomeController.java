c61871639996bc3d69a77e3b63a546fd510efe81
Dao Son <daoson2710@gmail.com>
2020-10-31 22:54:20 +0700

    Configuration db

---
+++ src/main/java/com/astronomy/controller/admin/HomeController.java
@@ -0,0 +1,13 @@
+package com.astronomy.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "admin/index";
+	}
+}
