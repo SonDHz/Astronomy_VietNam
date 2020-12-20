37ebbac2cf990049a1911557f14b01e9209f2022
Dao Son <daoson2710@gmail.com>
2020-12-20 09:02:01 +0700

    Email

---
+++ src/main/java/com/astronomy/constant/MailConstant.java
@@ -0,0 +1,14 @@
+package com.astronomy.constant;
+
+public class MailConstant {
+	
+	// Replace with your email here:  
+    public static final String MY_EMAIL = "sondhps09242@fpt.edu.vn";
+ 
+    // Replace password!!
+    public static final String MY_PASSWORD = "daohongson111";
+ 
+    // And receiver!
+//    public static final String FRIEND_EMAIL = "yourFriend@gmail.com";
+    
+}
---
+++ src/main/java/com/astronomy/mail/MailSender.java
@@ -0,0 +1,32 @@
+package com.astronomy.mail;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import com.astronomy.constant.MailConstant;
+
+@Configuration
+public class MailSender {
+	
+	@Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+ 
+        mailSender.setUsername(MailConstant.MY_EMAIL);
+        mailSender.setPassword(MailConstant.MY_PASSWORD);
+ 
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+ 
+        return mailSender;
+    }
+}
